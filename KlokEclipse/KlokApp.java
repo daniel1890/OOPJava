bb2d610a95998d70f547b72e3910e57c6996cc7f
Daniël <59751559+daniel1890@users.noreply.github.com>
2020-12-29 05:28:25 +0100

    Update KlokApp.java

--- KlokEclipse/KlokApp.java
+++ KlokEclipse/KlokApp.java
@@ -1,28 +1,30 @@
-package les07.klokineclipse;
+package main;
 
 import processing.core.PApplet;
 
 public class KlokApp extends PApplet {
 
 	public static void main(String[] args) {
-		PApplet.main(new String[] { "les07.klokineclipse.KlokApp" });
+		PApplet.main(new String[] { "main.KlokApp" });
+
 	}
 
 	Klok deKlok;
 
 	public void settings() {
-		size(400, 300);
+		size(400, 400);
 	}
 
 	public void setup() {
-		background(0);
+		background(255);
 		deKlok = new Klok(150, 100, 200, this);
+
 		deKlok.setTijd(23, 59);
-		deKlok.tekenKlok();
 	}
 
 	public void draw() {
 		deKlok.tik();
 		deKlok.tekenKlok();
 	}
+
 }
