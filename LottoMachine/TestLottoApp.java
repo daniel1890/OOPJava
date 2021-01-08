ae4b2282d87e653850d00426c3df837acfb90ada
Daniël <59751559+daniel1890@users.noreply.github.com>
2021-01-08 16:36:54 +0100

    Update TestLottoApp.java

--- LottoMachine/TestLottoApp.java
+++ LottoMachine/TestLottoApp.java
@@ -3,12 +3,10 @@ public class TestLottoApp {
 
 	public static void main(String[] args) {
 		Lottomachine machine = new Lottomachine();
-		machine.trekBallen();
-		machine.bord.sorteerBallen();
-		machine.bord.printScoreBord();
-		machine.trekBallen();
-		machine.bord.sorteerBallen();
-		machine.bord.printScoreBord();
+		for(int i = 0; i < 100; i++) {
+			machine.voerTrekkingUit();
+		}
+		
 	}
 
 }
